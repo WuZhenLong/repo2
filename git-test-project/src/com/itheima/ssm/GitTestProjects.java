c08d86bbff806fe086dda061d5dbf69a9a21b318
root <1446272382@qq.com>
2021-02-12 20:09:58 +0800

    把本地工程添加到Git

---
+++ git-test-project/src/com/itheima/ssm/GitTestProjects.java
@@ -0,0 +1,10 @@
+package com.itheima.ssm;
+
+public class GitTestProjects {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        System.out.println("hello2");
+        System.out.println("hello3");
+        System.out.println("hello4");
+    }
+}
